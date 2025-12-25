f64151ce4b339b054b7223fedcabf4146ce75d0c
Charles Lindberg <challe128@gmail.com>
2025-12-25 16:07:00 +0100

    Test

--- src/App.tsx
+++ src/App.tsx
@@ -65,7 +65,7 @@ export default function App() {
   }
 
   return (
-    <div className="app min-h-screen bg-gradient-to-br from-[#101318] to-[#1b1f27]">
+    <div className="app min-h-screen bg-gradient-to-br bg-[#101318]">
       <div className="max-w-4xl mx-auto p-4 sm:p-6">
         <div className="flex gap-2 mb-4">
           <button
