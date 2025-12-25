6385cdc373c691adbd45c9ee651a0fc3e6b94269
Charles Lindberg <challe128@gmail.com>
2025-12-25 15:29:36 +0100

    Test 9

--- src/components/WizardEntry.tsx
+++ src/components/WizardEntry.tsx
@@ -309,7 +309,7 @@ export function WizardEntry({ date, existingEntry, previousEntry, onSave, onCanc
           value={value}
           onChange={(e) => handleChange(currentField.id, e.target.value)}
           className="w-full px-8 py-8 text-center text-white bg-[#101318] border-4 border-white/20 rounded-3xl focus:outline-none focus:border-[#39ac63] transition-all min-h-64 resize-none text-2xl"
-          
+          autoFocus
           ref={inputRef as React.RefObject<HTMLTextAreaElement>}
         />
       );
